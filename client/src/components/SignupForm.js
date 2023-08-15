8fa926101c5814a412795932b7c0fc8bcb250b9d
Aly Oliveira <1alycia.oliveira@gmail.com>
2023-08-14 23:41:42 -0600

    fixing loginForm and SignupForm

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-import { createUser } from '../utils/API';
-import Auth from '../utils/auth';
+import AuthService from '../utils/auth';
+
+import { useMutation } from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 
 const SignupForm = () => {
   // set initial form state
@@ -11,6 +13,9 @@ const SignupForm = () => {
   const [validated] = useState(false);
   // set state for alert
   const [showAlert, setShowAlert] = useState(false);
+
+  // use mutation hook
+  const [addUser] = useMutation(ADD_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -28,15 +33,16 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const dataRes = await addUser({
+        variables: { 
+          username: userFormData.username,
+          email: userFormData.email,
+          password: userFormData.password,
+         },
+      });
+      
+      const token = dataRes.data.addUser.token;
+      AuthService.login(token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
